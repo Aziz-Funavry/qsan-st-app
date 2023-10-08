843b6eb745450b854ff0f8e7302b6b928678be3f
Aziz-Funavry <141824139+Aziz-Funavry@users.noreply.github.com>
2023-10-08 21:08:54 +0500

    Create networkplot.R

---
+++ networkplot.R
@@ -0,0 +1,25 @@
+require("networktools")
+library("MPsychoR")
+library("smacof")
+library("qgraph")
+library("psych")
+library("eigenmodel")
+library("dplyr")
+library("ggplot2")
+library("bootnet")
+library("qgraph")
+library("networktools")
+library("IsingSampler")
+library("IsingFit")
+library("bootnet")
+
+
+f'dt <- read.csv("{uploaded_file.name}", header=TRUE)'
+f'netdt1 <- select(dt, {columns_to_select_str})'
+net1 <- qgraph(cor_auto(netdt1), n = nrow(netdt1), lambda.min.ratio = 0.05, default = "EBICglasso", layout="spring", vsize = 16, gamma = 0.2, tuning = 0.2, refit = TRUE)
+
+
+
+png("qgraph_plot.png")
+qgraph(net1, maximum=0.29)
+dev.off()
